58889486e12f96426f9a789c009d943a79a32bda
Joe Stwart Valencia Renteria <stiwart94@gmail.com>
2026-08-15 17:34:11 -0500

    chore(deps): add magellan, aws-s3 flysystem and microsoft socialite

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\AuthServiceProvider;
+use SocialiteProviders\Manager\ServiceProvider;
 
 return [
     AppServiceProvider::class,
     AuthServiceProvider::class,
+    ServiceProvider::class,
 ];
